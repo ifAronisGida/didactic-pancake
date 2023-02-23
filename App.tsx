60058f70fbdf2dac3c337c3d10ca6f2e30f3f66b
a.gida <a.gida@mortoff.hu>
2023-02-23 21:37:09 +0100

    add react navigation

--- App.tsx
+++ App.tsx
@@ -1,17 +1,22 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Text, View } from 'react-native';
-import { NativeWindStyleSheet } from "nativewind";
+import LoginScreen from './src/components/Login';
+import { createStackNavigator } from '@react-navigation/stack';
 
 export default function App() {
+
+  const Stack = createStackNavigator();
+
   return (
-    <View>
-      <Text className="text-xl">Opeghn up</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Stack.Navigator>
+      <View className='flex-1 justify-center content-center self-center'>
+        <LoginScreen />
+        <Text>App</Text>
+        <StatusBar style="auto" />
+      </View>
+    </Stack.Navigator>
   );
 }
 
-NativeWindStyleSheet.setOutput({
-  default: "native",
-});
---
+++ src/components/Login.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const LoginScreen: React.FC = () => (
+  <View className="bg-purple-700">
+    <Text className='text-yellow-50'>Login</Text>
+  </View>
+)
+
+export default LoginScreen;
